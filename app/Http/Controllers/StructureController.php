5cdd3a09d4412cfe6525e01210aa11c123a7e5b0
Cyril <cyril.valette01@gmail.com>
2021-11-08 11:24:43 +0100

    mise en place de la page de structure du club

--- app/Http/Controllers/StructureController.php
+++ app/Http/Controllers/StructureController.php
@@ -2,14 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Stade;
+use App\Models\Centre_jeune;
 use Illuminate\Http\Request;
+use App\Models\Centre_entrainement;
 use Illuminate\Support\Facades\Auth;
 
 class StructureController extends Controller
 {
     public function structureAction(){
         $clubuser = Auth::user()->clubUser;
-        return view('structure',compact('clubuser'));
+        $stade = Stade::where('id', $clubuser->id)->first();
+        $centre_entrainement = Centre_entrainement::where('id', $clubuser->id)->first();
+        $centre_jeune = Centre_jeune::where('id', $clubuser->id)->first();
+        return view('structure',compact('clubuser','stade','centre_entrainement','centre_jeune'));
 
      }
 }
