586593f8814116e639543f79f7334d6c5720e43e
Cyril <cyril.valette01@gmail.com>
2021-11-08 11:49:28 +0100

    installation de notification et livewire

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,7 @@
 
     <!-- Styles -->
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    @livewireStyles
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -73,8 +74,8 @@
                 @yield('content')
             </div>
         </div>
-
     </div>
+    @livewireScripts
 </body>
 
 </html>
