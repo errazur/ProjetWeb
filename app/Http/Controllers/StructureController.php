d377963ae13dc0c8b471a49835d04d2b7ad313a6
errazur <systemhorscombat@gmail.com>
2021-11-15 16:34:57 +0100

    amelioration des structures de disponible

--- app/Http/Controllers/StructureController.php
+++ app/Http/Controllers/StructureController.php
@@ -12,9 +12,9 @@ class StructureController extends Controller
 {
     public function structureAction(){
         $clubuser = Auth::user()->clubUser;
-        $stade = Stade::where('id', $clubuser->id)->first();
-        $centre_entrainement = Centre_entrainement::where('id', $clubuser->id)->first();
-        $centre_jeune = Centre_jeune::where('id', $clubuser->id)->first();
+        $stade = Stade::where('id', $clubuser->stade_id)->first();
+        $centre_entrainement = Centre_entrainement::where('id', $clubuser->centre_entrainement_id)->first();
+        $centre_jeune = Centre_jeune::where('id', $clubuser->centre_jeune_id)->first();
         return view('structure',compact('clubuser','stade','centre_entrainement','centre_jeune'));
 
      }
--- app/Http/Livewire/Parc.php
+++ app/Http/Livewire/Parc.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Club;
 use Livewire\Component;
+use Illuminate\Support\Facades\Auth;
 
 class Parc extends Component
 {
@@ -13,29 +15,47 @@ class Parc extends Component
     public $centre_jeune;
 
     public function upgraderStade(){
+        $clubuser = $this->clubuser;
+        $argentClub = $clubuser->Argent;
+        $stadeLevel = $clubuser->stade_id;
 
-        if($this->clubuser->Argent >= $this->stade->prix_amelioriation){
-            dd($this->clubuser);
+        if($clubuser->Argent >= $this->stade->prix_amelioriation){
+            Club::where('id', Auth::id())
+            ->update([  'Argent' => $argentClub - $this->stade->prix_amelioriation,
+                        'stade_id' => $stadeLevel + 1
+                    ]);
         }   else {
-                $this->emit('flash', 'tu n\'a pas l\'argent ncessaire pour faire l\'amélioration du stade', 'error');
+                $this->emit('flash', 'Tu n\'a pas l\'argent ncessaire pour faire l\'amélioration du stade.', 'error');
         }
     }
 
     public function upgraderEntrainement(){
+        $clubuser = $this->clubuser;
+        $argentClub = $clubuser->Argent;
+        $entrainementLevel = $clubuser->centre_entrainement_id;
 
-        if($this->clubuser->Argent >= $this->centre_entrainement->prix_amelioriation){
-            dd($this->clubuser);
+        if($clubuser->Argent >= $this->centre_entrainement->prix_amelioriation){
+            Club::where('id', Auth::id())
+            ->update([  'Argent' => $argentClub - $this->centre_entrainement->prix_amelioriation,
+                        'centre_entrainement_id' => $entrainementLevel + 1
+                    ]);
         }   else{
-                $this->emit('flash', 'tu n\'a pas l\'argent ncessaire pour faire l\'amélioration du centre d\'entrainement', 'error');
+                $this->emit('flash', 'Tu n\'a pas l\'argent ncessaire pour faire l\'amélioration du centre d\'entrainement.', 'error');
         }
     }
 
     public function upgraderJeune(){
+        $clubuser = $this->clubuser;
+        $argentClub = $clubuser->Argent;
+        $centrejeuneLevel = $clubuser->centre_jeune_id;
 
-        if($this->clubuser->Argent >= $this->centre_jeune->prix_amelioriation){
-            dd($this->clubuser);
+        if($clubuser->Argent >= $this->centre_jeune->prix_amelioriation){
+            Club::where('id', Auth::id())
+            ->update([  'Argent' => $argentClub - $this->centre_jeune->prix_amelioriation,
+                        'centre_jeune_id' => $centrejeuneLevel + 1
+                    ]);
         }   else{
-                $this->emit('flash', 'tu n\'a pas l\'argent ncessaire pour faire l\'amélioration du centre de jeune', 'error');
+                $this->emit('flash', 'Tu n\'a pas l\'argent ncessaire pour faire l\'amélioration du centre de jeune.', 'error');
         }
     }
 
