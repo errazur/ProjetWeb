82387dfe0633391796873a94c6fa453ef986abb1
errazur <systemhorscombat@gmail.com>
2021-12-08 18:30:46 +0100

    mise en place de la vue de la tactique sur le terrain des joueurs

--- app/Http/Livewire/Tactique.php
+++ app/Http/Livewire/Tactique.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Auth;
 class Tactique extends Component
 {
 
-    public $poste = ['','GC', 'DG','DC','DC', 'DD','MG','MC','MC', 'MD', 'BU','BU', 'REM1','REM2','REM3','REM4'];
+    public $poste = ['','GC', 'DD','DC','DC', 'DG','MD','MC','MC', 'MG', 'BU','BU', 'REM1','REM2','REM3','REM4'];
 
     public function updateJoueurOrder($items)
     {
@@ -27,6 +27,19 @@ class Tactique extends Component
     {
         $clubuser = Auth::user()->clubUser;
         $joueurs = Joueur::where('club_id', $clubuser->id)->orderBy('order_position', 'asc')->get();
-        return view('livewire.tactique', compact('clubuser','joueurs'));
+
+        $joueur1 = Joueur::where('club_id', $clubuser->id)->where('order_position',1)->first();
+        $joueur2 = Joueur::where('club_id', $clubuser->id)->where('order_position',2)->first();
+        $joueur3 = Joueur::where('club_id', $clubuser->id)->where('order_position',3)->first();
+        $joueur4 = Joueur::where('club_id', $clubuser->id)->where('order_position',4)->first();
+        $joueur5 = Joueur::where('club_id', $clubuser->id)->where('order_position',5)->first();
+        $joueur6 = Joueur::where('club_id', $clubuser->id)->where('order_position',6)->first();
+        $joueur7 = Joueur::where('club_id', $clubuser->id)->where('order_position',7)->first();
+        $joueur8 = Joueur::where('club_id', $clubuser->id)->where('order_position',8)->first();
+        $joueur9 = Joueur::where('club_id', $clubuser->id)->where('order_position',9)->first();
+        $joueur10 = Joueur::where('club_id', $clubuser->id)->where('order_position',10)->first();
+        $joueur11 = Joueur::where('club_id', $clubuser->id)->where('order_position',11)->first();
+
+        return view('livewire.tactique', compact('clubuser','joueurs','joueur1','joueur2','joueur3','joueur4','joueur5','joueur6','joueur7','joueur8','joueur9','joueur10','joueur11'));
     }
 }
--- resources/views/livewire/tactique.blade.php
+++ resources/views/livewire/tactique.blade.php
@@ -3,26 +3,113 @@
     <div class="flex flex-1 flex-col shadow-lg rounded-md m-2">
 
         <div class="flex flex-1 bg-center bg-terrain w-full bg-contain bg-no-repeat">
-            <div class="grid grid-cols-4 gap-4 w-full">
+            <div class="grid grid-cols-6 gap-4 w-full m-3">
 
-                <div class="z-1">1</div>
-                <div class="z-1">2</div>
-                <div class="z-1">3</div>
-                <div class="z-1">4</div>
-                <div class="z-1">5</div>
-                <div class="z-1">6</div>
-                <div class="z-1">7</div>
-                <div class="z-1">8</div>
-                <div class="z-1">9</div>
-                <div class="z-1">10</div>
-                <div class="z-1">11</div>
+                <div class="z-1 col-span-3">
+                    <div class="flex justify-end">
+                        <div class="flex flex-col items-center">
+                            <img src="{{ URL::asset('images/person.svg') }}" alt="" class="img-responsive w-12">
+                            {{ $joueur11->nom }}
+                        </div>
 
+                    </div>
+                </div>
+                <div class="z-1 col-start-4 col-span-3">
+                    <div class="flex justify-start">
+                        <div class="flex flex-col items-center">
+                            <img src="{{ URL::asset('images/person.svg') }}" alt="" class="img-responsive w-12">
+                            {{ $joueur10->nom }}
+                        </div>
+
+                    </div>
+                </div>
+                <div class="z-1 col-start-2 ">
+                    <div class="flex justify-start ml-4">
+                        <div class="flex flex-col items-center">
+                            <img src="{{ URL::asset('images/person.svg') }}" alt="" class="img-responsive w-12">
+                            {{ $joueur9->nom }}
+                        </div>
+
+                    </div>
+                </div>
+                <div class="z-1 col-start-3">
+                    <div class="flex justify-center">
+                        <div class="flex flex-col items-center">
+                            <img src="{{ URL::asset('images/person.svg') }}" alt="" class="img-responsive w-12">
+                            {{ $joueur8->nom }}
+                        </div>
+
+                    </div>
+                </div>
+                <div class="z-1 col-start-4">
+                    <div class="flex justify-center">
+                        <div class="flex flex-col items-center">
+                            <img src="{{ URL::asset('images/person.svg') }}" alt="" class="img-responsive w-12">
+                            {{ $joueur7->nom }}
+                        </div>
+
+                    </div>
+                </div>
+                <div class="z-1 col-start-5">
+                    <div class="flex justify-end mr-4">
+                        <div class="flex flex-col items-center">
+                            <img src="{{ URL::asset('images/person.svg') }}" alt="" class="img-responsive w-12">
+                            {{ $joueur6->nom }}
+                        </div>
+
+                    </div>
+                </div>
+                <div class="z-1 col-start-2">
+                    <div class="flex justify-center">
+                        <div class="flex flex-col items-center">
+                            <img src="{{ URL::asset('images/person.svg') }}" alt="" class="img-responsive w-12">
+                            {{ $joueur5->nom }}
+                        </div>
+
+                    </div>
+                </div>
+                <div class="z-1 col-start-3">
+                    <div class="flex justify-center">
+                        <div class="flex flex-col items-center">
+                            <img src="{{ URL::asset('images/person.svg') }}" alt="" class="img-responsive w-12">
+                            {{ $joueur4->nom }}
+                        </div>
+
+                    </div>
+                </div>
+                <div class="z-1 col-start-4">
+                    <div class="flex justify-center">
+                        <div class="flex flex-col items-center">
+                            <img src="{{ URL::asset('images/person.svg') }}" alt="" class="img-responsive w-12">
+                            {{ $joueur3->nom }}
+                        </div>
+
+                    </div>
+                </div>
+                <div class="z-1 col-start-5">
+                    <div class="flex justify-center">
+                        <div class="flex flex-col items-center">
+                            <img src="{{ URL::asset('images/person.svg') }}" alt="" class="img-responsive w-12">
+                            {{ $joueur2->nom }}
+                        </div>
+
+                    </div>
+                </div>
+                <div class="z-1 col-span-full">
+                    <div class="flex justify-center">
+                        <div class="flex flex-col items-center">
+                            <img src="{{ URL::asset('images/person.svg') }}" alt="" class="img-responsive w-12">
+                            {{ $joueur1->nom }}
+                        </div>
+
+                    </div>
+                </div>
 
             </div>
 
         </div>
 
-        <div class="flex flex-1">
+        <div class="flex flex-1 bg-gray-900 rounded-md m-2">
 
         </div>
 
@@ -34,15 +121,15 @@
                 Liste de l'effectif du club :
             </div>
             <div class="flex flex-1 justify-center">
-                <table class="table table-auto border-separate border m-2 text-gray-400">
-                    <thead>
+                <table class="min-w-full divide-y divide-gray-200 m-2 text-gray-400">
+                    <thead class="text-gray-200">
                         <tr class="">
-                            <th scope="col">!</th>
-                            <th scope="col">#</th>
-                            <th scope="col">Nom du joueurs</th>
-                            <th scope="col">Poste</th>
-                            <th scope="col">Endurance</th>
-                            <th scope="col">Forme</th>
+                            <th class="py-3 uppercase tracking-wider" scope="col">!</th>
+                            <th class="py-3 uppercase tracking-wider" scope="col">#</th>
+                            <th class="py-3 uppercase tracking-wider" scope="col">Nom du joueurs</th>
+                            <th class="py-3 uppercase tracking-wider" scope="col">Poste</th>
+                            <th class="py-3 uppercase tracking-wider" scope="col">Endurance</th>
+                            <th class="py-3 uppercase tracking-wider" scope="col">Forme</th>
                         </tr>
                     </thead>
                     <tbody wire:sortable="updateJoueurOrder">
@@ -50,7 +137,7 @@
                             <tr wire:sortable.item="{{ $joueur->id }}" wire:key="joueur-{{ $joueur->id }}"
                                 wire:sortable.handle style="width: 10px; cursor: move;" class="hover:shadow-md">
 
-                                <th scope="row">{{$poste[$loop->iteration]}}</th>
+                                <th scope="row">{{ $poste[$loop->iteration] }}</th>
                                 <th scope="row">{{ $loop->iteration }}</th>
                                 <td><a class="hover:underline" href="{{ route('joueur', $joueur) }}">
                                         {{ $joueur->nom }} {{ $joueur->prenom }}
